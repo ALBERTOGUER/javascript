16aaef46a87df47e04788695d7e74e36c1976677
alberto3147 <alberto.perez.1509@outlook.com>
2019-09-05 19:48:22 -0500

    Se finalizo ejemplo 8 en practicas estructuras condocionales simples (faltaba de terminarse)

--- Practicas_estructuras_condicionales_simples/ejemplo8/app.js
+++ Practicas_estructuras_condicionales_simples/ejemplo8/app.js
@@ -1,28 +1,34 @@
-let btn= document.getElementById('btnCalcular');
-let nombre1=document.getElementById('nombre1').value;
-let nombre2=document.getElementById('nombre2').value;
-let edad1=document.getElementById('eda1');
-let edad2=document.getElementById('edad2');
+let btn = document.getElementById('btnCalcular');
+let edad1 = document.getElementById('edad1');
+let edad2 = document.getElementById('edad2');
 
 addevent();
 
-function addevent(){
-btn.addEventListener('click', hola);
-edad1.addEventListener('blur',revision);
-edad2.addEventListener('blur',revision);
+function addevent() {
+    btn.addEventListener('click', hola);
+    edad1.addEventListener('blur', revision);
+    edad2.addEventListener('blur', revision);
 
 }
 
-function revision(event){
-    if(event.target.value){
+
+function revision(event) {
+    if (isNaN(event.target.value)) {
 
         event.target.focus();
     }
 }
 
-function hola(){ 
-    
-    if(Number(edad1.value) > Number(edad2.value)){
+function hola() {
+    let nombre1 = document.getElementById('1').value;
+    let nombre2 = document.getElementById('2').value;
+    let imprimir = document.getElementById('resultado');
+    console.log = nombre1;
+    if (Number(edad1.value) > Number(edad2.value)) {
+        imprimir.innerText = nombre1;
+    }
+    else {
+        imprimir.innerText = nombre2;
 
     }
 
